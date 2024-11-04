4e09870f557e8af7a180e32f74f814a50766ee36
Cedric Silene <cedric.silene@proton.me>
2024-11-04 19:02:27 +0100

    feat: displaying photographer's data on photographer.html

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,7 +4,9 @@ async function autorun() {
     // Récupération des données des photographes du fichier json
     const { photographers } = await getPhotographers();
     const photographer = photographers.find(entry => entry.id == id);
+    // Stockage des données du photographe dans le localStorage
     dataStorage(photographer);
+    getCardDOM();
 }
 
 async function getPhotographers() {
@@ -21,8 +23,30 @@ function dataStorage(data) {
     window.localStorage.setItem("city", data.city);
     window.localStorage.setItem("country", data.country);
     window.localStorage.setItem("tagline", data.tagline);
-    window.localStorage.setItem("portrait", data.portrait);
-    console.log(id);
+    window.localStorage.setItem("portrait", data.portrait);    
+}
+
+function getData(varName) {
+    varName = window.localStorage.getItem(varName);
+    return varName;
+}
+
+function getCardDOM() {
+    const name = getData("name");
+    const photographerName = document.getElementById("name");
+    photographerName.innerText = name;
+    const city = getData("city");
+    const country = getData("country");
+    const location = document.getElementById("location");
+    location.innerText = city+", "+country;
+    const tagline = getData("tagline");
+    const slogan = document.getElementById("tagline");
+    slogan.innerText = tagline;
+    const portrait = getData("portrait");
+    const image = document.getElementById("portrait");
+    image.setAttribute("src", `assets/photographers/${portrait}`);
+    image.setAttribute("alt", `portrait de ${name}`);    
+    return;
 }
 
 autorun();
