c192132b377f7de27996ca1d9448a2c0877b01a9
Cedric Silene <cedric.silene@proton.me>
2024-11-14 06:06:19 +0100

    feat: (photographer.html) display photographers info

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -7,6 +7,7 @@ async function autorun() {
     // Stockage des données du photographe dans le localStorage
     dataStorage(photographer);
     getCardDOM();
+    getPortfolio(id);
 }
 
 async function getPhotographers() {
@@ -51,6 +52,43 @@ function getCardDOM() {
 
 function dropdownMenu() {
     document.getElementById("Dropdown").classList.toggle("show");
-}  
+}
+
+async function getPortfolio(id) {
+    const { media } = await getPhotographers();
+    const mediaList = media.filter(entry => entry.photographerId == id);
+    const grid = document.getElementById("grid");
+    mediaList.forEach(element => {
+        const grid = document.createElement( 'article' );
+        const link = document.createElement( 'a' );
+    });
+    console.log(mediaList);
+    /* const article = document.createElement( 'article' );
+    const link = document.createElement( 'a' );
+    link.setAttribute("href", "photographer.html#"+id);
+    article.appendChild(link);
+    const div = document.createElement( 'div' );
+    div.setAttribute("class", "image");
+    link.appendChild(div);
+    const img = document.createElement( 'img' );
+    img.setAttribute("src", picture);
+    div.appendChild(img);
+    const h2 = document.createElement( 'h2' );
+    h2.textContent = name;        
+    link.appendChild(h2);
+    const location = document.createElement( 'p' );
+    location.setAttribute("class", "city");
+    location.textContent = city+", "+country;
+    article.appendChild(location);
+    const slogan = document.createElement( 'p' );
+    slogan.setAttribute("class", "tagline");
+    slogan.textContent = tagline;
+    article.appendChild(slogan);
+    const cost = document.createElement( 'p' );
+    cost.setAttribute("class", "price");
+    cost.textContent = price+"€/jour";
+    article.appendChild(cost);
+    return (article); */
+}
 
 autorun();
