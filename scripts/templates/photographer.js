54ac24890ec340c070cf723a44c0f612840d996c
Cedric Silene <cedric.silene@proton.me>
2025-01-21 09:36:06 +0100

    Restart project

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,35 +1,17 @@
 function photographerTemplate(data) {
-    const { id, name, city, country, portrait, price, tagline } = data;
+    const { name, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const link = document.createElement( 'a' );
-        link.setAttribute("href", "photographer.html#"+id);
-        article.appendChild(link);
-        const div = document.createElement( 'div' );
-        div.setAttribute("class", "image");
-        link.appendChild(div);
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture);
-        div.appendChild(img);
+        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
-        h2.textContent = name;        
-        link.appendChild(h2);
-        const location = document.createElement( 'p' );
-        location.setAttribute("class", "city");
-        location.textContent = city+", "+country;
-        article.appendChild(location);
-        const slogan = document.createElement( 'p' );
-        slogan.setAttribute("class", "tagline");
-        slogan.textContent = tagline;
-        article.appendChild(slogan);
-        const cost = document.createElement( 'p' );
-        cost.setAttribute("class", "price");
-        cost.textContent = price+"€/jour";
-        article.appendChild(cost);
+        h2.textContent = name;
+        article.appendChild(img);
+        article.appendChild(h2);
         return (article);
     }
-    return { name, city, country, portrait, price, tagline, getUserCardDOM }
+    return { name, picture, getUserCardDOM }
 }
